478fcd5cb46172df5b9c2327d70d677e1a8bc8e6
unknown <csmcc2@hotmail.com>
2024-12-17 10:59:19 +0800

    text hash

---
+++ hashx/text/text.go
@@ -0,0 +1,32 @@
+package text
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
+type TextBasic struct{}
+
+func (t *TextBasic) Base64Encode(s []byte) string {
+	return base64.StdEncoding.EncodeToString(s)
+}
+
+func (t *TextBasic) Base64Decode(s string) (string, error) {
+	ds, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+	return string(ds), nil
+}
+
+func (t *TextBasic) HexEncode(s []byte) string {
+	return hex.EncodeToString(s)
+}
+
+func (t *TextBasic) HexDecode(s string) (string, error) {
+	ds, err := hex.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+	return string(ds), nil
+}
